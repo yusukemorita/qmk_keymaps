f6220a765ac8c4187e624eb5652362f199081bd2
yusukemorita <yusuke.ppq@gmail.com>
2024-04-18 23:47:48 +0200

    edit more keys

--- keyball/keyball44/keymaps/default/keymap.c
+++ keyball/keyball44/keymaps/default/keymap.c
@@ -40,15 +40,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // keymap for default
   [0] = LAYOUT_universal(
     XXXXXXX  , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                        KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , XXXXXXX  ,
-    XXXXXXX  , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                        KC_H     , KC_J     , KC_K     , KC_L     , KC_COLON , XXXXXXX  ,
+    XXXXXXX  , LCTL_T(KC_A), KC_S  , KC_D     , KC_F     , KC_G     ,                                        KC_H     , KC_J     , KC_K     , KC_L     , RCTL_T(KC_COLON), XXXXXXX,
     XXXXXXX  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                                        KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLASH , XXXXXXX  ,
                XXXXXXX  , XXXXXXX  , KC_LGUI  , LT(1,KC_MS_BTN1), LSFT(KC_SPACE),                         RSFT(KC_ENT), LT(2,KC_MS_BTN1), KC_RALT,       KC_RALT  , KC_MS_BTN1
   ),
 
   [1] = LAYOUT_universal(
     XXXXXXX  ,  KC_1    , KC_2     , KC_3    , KC_4     , KC_5     ,                                         KC_6     , KC_7     , KC_8     , KC_9     , KC_0     , XXXXXXX  ,
-    XXXXXXX  ,  _______ , _______  , KC_UP   , KC_ENT   , KC_DEL   ,                                         KC_LEFT  , KC_DOWN  , KC_UP    , KC_RIGHT , XXXXXXX  , XXXXXXX  ,
-    XXXXXXX  ,  _______ , KC_LEFT  , KC_DOWN , KC_RGHT  , KC_BSPC  ,                                         KC_DEL   , KC_BSPC  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  ,
+    XXXXXXX  ,  _______ , _______  , _______ , _______  , _______  ,                                         KC_LEFT  , KC_DOWN  , KC_UP    , KC_RIGHT , XXXXXXX  , XXXXXXX  ,
+    XXXXXXX  ,  _______ , _______  , _______ , _______  , _______  ,                                         KC_DEL   , KC_BSPC  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  ,
                   _______  , _______ , _______  ,         _______  , _______  ,                   KC_ESC_AND_ENG , _______  , _______       , XXXXXXX  , _______
   ),
 
