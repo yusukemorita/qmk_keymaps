1ebfdadb6ec49790b2136195fcaaaa0c64e95bb8
yusukemorita <yusuke.ppq@gmail.com>
2024-04-18 23:43:10 +0200

    add esc and eng macro

--- keyball/keyball44/keymaps/default/keymap.c
+++ keyball/keyball44/keymaps/default/keymap.c
@@ -20,6 +20,20 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "quantum.h"
 
+enum custom_keycodes {
+  KC_ESC_AND_ENG = SAFE_RANGE,
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch(keycode) {
+    case KC_ESC_AND_ENG:
+      if (record->event.pressed) {
+        SEND_STRING(SS_TAP(KC_ESC)SS_TAP(KC_LANGUAGE_2));
+      }
+      break;
+  }
+
+
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // keymap for default
@@ -34,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     XXXXXXX  ,  KC_1    , KC_2     , KC_3    , KC_4     , KC_5     ,                                         KC_6     , KC_7     , KC_8     , KC_9     , KC_0     , XXXXXXX  ,
     XXXXXXX  ,  _______ , _______  , KC_UP   , KC_ENT   , KC_DEL   ,                                         KC_LEFT  , KC_DOWN  , KC_UP    , KC_RIGHT , XXXXXXX  , XXXXXXX  ,
     XXXXXXX  ,  _______ , KC_LEFT  , KC_DOWN , KC_RGHT  , KC_BSPC  ,                                         KC_DEL   , KC_BSPC  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  ,
-                  _______  , _______ , _______  ,         _______  , _______  ,                   _______  , _______  , _______       , XXXXXXX  , XXXXXXX
+                  _______  , _______ , _______  ,         _______  , _______  ,                   KC_ESC_AND_ENG , _______  , _______       , XXXXXXX  , _______
   ),
 
   [2] = LAYOUT_universal(
