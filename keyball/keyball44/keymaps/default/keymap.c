022cae7c5002b699fc3cfe7c0a11e572d8539615
yusukemorita <yusuke.ppq@gmail.com>
2024-04-19 10:41:54 +0200

    add missing keys

--- keyball/keyball44/keymaps/default/keymap.c
+++ keyball/keyball44/keymaps/default/keymap.c
@@ -53,12 +53,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     XXXXXXX  , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                        KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , XXXXXXX  ,
     XXXXXXX  , LCTL_T(KC_A), KC_S  , KC_D     , KC_F     , KC_G     ,                                        KC_H     , KC_J     , KC_K     , KC_L     , RCTL_T(KC_COLON), XXXXXXX,
     XXXXXXX  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                                        KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLASH , XXXXXXX  ,
-    XXXXXXX  , XXXXXXX  ,            KC_LGUI  , LT(1,CLICK), LSFT(KC_SPACE),                            RSFT(KC_ENT), LT(2,CLICK),            _______  , KC_RALT  , CLICK
+    XXXXXXX  , XXXXXXX  ,            KC_LGUI  , LT(1,CLICK), LSFT_T(KC_SPACE),                         RSFT_T(KC_ENT) , LT(2,CLICK),          _______  , KC_RALT  , CLICK
   ),
 
   [1] = LAYOUT_universal(
     XXXXXXX  , KC_1     , KC_2     , KC_3    , KC_4     , KC_5     ,                                         KC_6     , KC_7     , KC_8     , KC_9     , KC_0     , XXXXXXX  ,
-    XXXXXXX  , _______  , _______  , _______ , _______  , _______  ,                                         KC_LEFT  , KC_DOWN  , KC_UP    , KC_RIGHT , XXXXXXX  , XXXXXXX  ,
+    XXXXXXX  , _______  , _______  , SCRL_MO , _______  , _______  ,                                         KC_LEFT  , KC_DOWN  , KC_UP    , KC_RIGHT , XXXXXXX  , XXXXXXX  ,
     XXXXXXX  , _______  , _______  , _______ , _______  , _______  ,                                         KC_DEL   , KC_BSPC  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  ,
     _______  , _______  ,            _______ , _______  , _______  ,                                   KC_ESC_AND_ENG , LT(3,_______) ,       _______  , _______  , _______
   ),
