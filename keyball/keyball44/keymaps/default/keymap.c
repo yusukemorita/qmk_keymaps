2ba7027b1b86c8577151c00280dca0faccdcff0f
yusukemorita <yusuke.ppq@gmail.com>
2024-04-19 08:36:12 +0200

    complete keymap

--- keyball/keyball44/keymaps/default/keymap.c
+++ keyball/keyball44/keymaps/default/keymap.c
@@ -20,6 +20,17 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "quantum.h"
 
+// aliases
+
+#define JAP KC_LANG1
+#define ENG KC_LANG2
+
+#define CPI_INC CPI_I100
+#define CPI_DEC CPI_D100
+#define CLICK KC_MS_BTN1
+
+// macro
+
 enum custom_keycodes {
   KC_ESC_AND_ENG = SAFE_RANGE
 };
@@ -42,28 +53,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     XXXXXXX  , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                        KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , XXXXXXX  ,
     XXXXXXX  , LCTL_T(KC_A), KC_S  , KC_D     , KC_F     , KC_G     ,                                        KC_H     , KC_J     , KC_K     , KC_L     , RCTL_T(KC_COLON), XXXXXXX,
     XXXXXXX  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                                        KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLASH , XXXXXXX  ,
-               XXXXXXX  , XXXXXXX  , KC_LGUI  , LT(1,KC_MS_BTN1), LSFT(KC_SPACE),                         RSFT(KC_ENT), LT(2,KC_MS_BTN1), KC_RALT,       KC_RALT  , KC_MS_BTN1
+    XXXXXXX  , XXXXXXX  ,            KC_LGUI  , LT(1,CLICK), LSFT(KC_SPACE),                            RSFT(KC_ENT), LT(2,CLICK),            _______  , KC_RALT  , CLICK
   ),
 
   [1] = LAYOUT_universal(
-    XXXXXXX  ,  KC_1    , KC_2     , KC_3    , KC_4     , KC_5     ,                                         KC_6     , KC_7     , KC_8     , KC_9     , KC_0     , XXXXXXX  ,
-    XXXXXXX  ,  _______ , _______  , _______ , _______  , _______  ,                                         KC_LEFT  , KC_DOWN  , KC_UP    , KC_RIGHT , XXXXXXX  , XXXXXXX  ,
-    XXXXXXX  ,  _______ , _______  , _______ , _______  , _______  ,                                         KC_DEL   , KC_BSPC  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  ,
-                  _______  , _______ , _______  ,         _______  , _______  ,                   KC_ESC_AND_ENG , LT(3,_______)  , _______       , _______  , _______
+    XXXXXXX  , KC_1     , KC_2     , KC_3    , KC_4     , KC_5     ,                                         KC_6     , KC_7     , KC_8     , KC_9     , KC_0     , XXXXXXX  ,
+    XXXXXXX  , _______  , _______  , _______ , _______  , _______  ,                                         KC_LEFT  , KC_DOWN  , KC_UP    , KC_RIGHT , XXXXXXX  , XXXXXXX  ,
+    XXXXXXX  , _______  , _______  , _______ , _______  , _______  ,                                         KC_DEL   , KC_BSPC  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  ,
+    _______  , _______  ,            _______ , _______  , _______  ,                                   KC_ESC_AND_ENG , LT(3,_______) ,       _______  , _______  , _______
   ),
 
   [2] = LAYOUT_universal(
-    XXXXXXX , KC_EXCLAIM, KC_DOUBLE_QUOTE, KC_HASH, KC_DOLLAR, KC_PERCENT,                                         S(KC_9)  , S(KC_1)  , S(KC_6)  , KC_LBRC  , S(KC_4)  , XXXXXXX  ,
-    XXXXXXX  ,KC_TAB, KC_4     , KC_5    , KC_6     , KC_RBRC  ,                                         KC_NUHS  , KC_MINS  , S(KC_EQL), S(KC_3)  , KC_QUOT  , XXXXXXX  ,
-    XXXXXXX  ,S(KC_MINS), KC_1     , KC_2    , KC_3     ,S(KC_RBRC),                                        S(KC_NUHS),S(KC_INT1), KC_EQL   ,S(KC_LBRC),S(KC_SLSH), XXXXXXX  ,
-                  KC_0     , KC_DOT  , _______  ,         _______  , _______  ,                   KC_DEL   , _______  , _______       , _______  , _______
+    XXXXXXX  , KC_EXCLAIM, KC_DQT  , KC_HASH , KC_DOLLAR, KC_PERCENT,                                        KC_AMPR  , KC_ASTR  , KC_LPRN  , KC_RPRN  , KC_CIRC  , XXXXXXX  ,
+    XXXXXXX  , KC_TAB   , KC_QUOTE , ENG     , JAP      , KC_AT    ,                                         KC_MINUS , KC_EQUAL , KC_LCBR  , KC_RCBR  , KC_SCLN  , XXXXXXX  ,
+    XXXXXXX  , _______  , KC_GRAVE , KC_TILDE, KC_PIPE  , _______  ,                                     KC_UNDERSCORE, KC_PLUS  , KC_EQL   , KC_LBRC  , KC_RBRC  , XXXXXXX  ,
+    _______  , _______  ,            _______ , LT(3,_______) , _______  ,                                    _______  , _______  ,            _______  , _______  , _______
   ),
 
   [3] = LAYOUT_universal(
-    XXXXXXX  , AML_TO   , AML_I50  , AML_D50  , _______  , _______  ,                                        RGB_M_P  , RGB_M_B  , RGB_M_R  , RGB_M_SW , RGB_M_SN , XXXXXXX  ,
-    XXXXXXX  , RGB_HUI  , RGB_SAI  , RGB_VAI  , _______  , SCRL_DVI ,                                        RGB_M_X  , RGB_M_G  , RGB_M_T  , RGB_M_TW , _______  , XXXXXXX  ,
-    XXXXXXX  , RGB_HUD  , RGB_SAD  , RGB_VAD  , _______  , SCRL_DVD ,                                        CPI_D1K  , CPI_D100 , CPI_I100 , CPI_I1K  , _______  , XXXXXXX  ,
-                  QK_BOOT  , KBC_RST  , _______  ,        _______  , _______  ,                   _______  , _______  , _______       , KBC_RST  , QK_BOOT
+    XXXXXXX  , _______  , _______  , _______  , _______  , _______  ,                                        _______  , _______  , _______  , _______  , _______  , XXXXXXX  ,
+    XXXXXXX  , _______  , _______  , _______  , _______  , CPI_INC  ,                                        _______  , _______  , _______  , _______  , _______  , XXXXXXX  ,
+    XXXXXXX  , _______  , _______  , _______  , _______  , CPI_DEC  ,                                        _______  , _______  , _______  , _______  , _______  , XXXXXXX  ,
+    XXXXXXX  , _______  , _______  ,            _______  , _______  ,                                        _______  , _______  ,            _______  , _______  , XXXXXXX
   ),
 };
 // clang-format on
