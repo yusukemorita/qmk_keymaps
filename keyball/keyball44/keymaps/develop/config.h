9593a495fb8f9392456087dbc16ca2c1835d1896
yusukemorita <yusuke.ppq@gmail.com>
2024-04-19 11:06:48 +0200

    adjust scroll default

--- keyball/keyball44/keymaps/develop/config.h
+++ keyball/keyball44/keymaps/develop/config.h
@@ -34,4 +34,4 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // custom defaults
 #define KEYBALL_CPI_DEFAULT 800
-#define KEYBALL_SCROLL_DIV_DEFAULT 8
+#define KEYBALL_SCROLL_DIV_DEFAULT 32
