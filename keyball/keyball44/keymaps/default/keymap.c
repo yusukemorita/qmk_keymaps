97033e1f01fbf1f18cbcf7cb91551f31f13ff181
yusukemorita <yusuke.ppq@gmail.com>
2024-04-20 12:21:14 +0200

    try removing tap modifier for k

--- keyball/keyball44/keymaps/default/keymap.c
+++ keyball/keyball44/keymaps/default/keymap.c
@@ -86,7 +86,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // keymap for default
   [0] = LAYOUT_universal(
     XXXXXXX  , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                        KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , XXXXXXX  ,
-    XXXXXXX  , LCTL_T(KC_A), KC_S  , KC_D     , KC_F     , KC_G     ,                                        KC_H     , KC_J, MT(SCRL_MO, KC_K), RCTL_T(KC_L) , KC_COLON, XXXXXXX,
+    XXXXXXX  , LCTL_T(KC_A), KC_S  , KC_D     , KC_F     , KC_G     ,                                        KC_H     , KC_J     , KC_K  , RCTL_T(KC_L), KC_COLON , XXXXXXX  ,
     XXXXXXX  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                                        KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLASH , XXXXXXX  ,
     XXXXXXX  , XXXXXXX  ,            KC_LGUI  , LT(1,CLICK), LSFT_T(KC_SPACE),                         RSFT_T(KC_ENT) , LT(2,CLICK),          _______  , KC_RALT  , CLICK
   ),
