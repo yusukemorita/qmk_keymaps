da308c5d98d16688a39292ee3d06c29f4ac2f504
yusukemorita <yusuke.ppq@gmail.com>
2024-04-22 20:21:55 +0200

    try making tapping term even shorter

--- keyball/keyball44/keymaps/default/config.h
+++ keyball/keyball44/keymaps/default/config.h
@@ -43,5 +43,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define AUTO_MOUSE_DEFAULT_LAYER 3
 #define AUTO_MOUSE_TIME 500
 
-#define TAPPING_TERM 150
+#define TAPPING_TERM 120
 #define PERMISSIVE_HOLD_PER_KEY
