0d391d7a4f0865814217731a4ae743e6d4b7b2b7
Yusuke Morita <yusuke.ppq@gmail.com>
2024-04-24 19:50:31 +0200

    try permissive hold  (#10)
    * try deleting permissive hold per key, not sure if it's doing anything
    
    * try defining permissive hold

--- keyball/keyball44/keymaps/default/config.h
+++ keyball/keyball44/keymaps/default/config.h
@@ -43,6 +43,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define AUTO_MOUSE_DEFAULT_LAYER 3
 #define AUTO_MOUSE_TIME 500
 
-#define PERMISSIVE_HOLD_PER_KEY
 #define TAPPING_TERM_PER_KEY
 #define TAPPING_TERM 300
+#define PERMISSIVE_HOLD
