f60c348f38318ea8c503ab8f849f5856e95336b2
Yusuke Morita <yusuke.ppq@gmail.com>
2024-04-25 18:16:51 +0200

    separate mouse layer and layer 3 (#11)
    * separate mouse layer and layer 3
    
    * delete unused aliases
    
    * define aliases for screenshot keys
    
    * fix wrong alias

--- keyball/keyball44/keymaps/default/keymap.c
+++ keyball/keyball44/keymaps/default/keymap.c
@@ -25,10 +25,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define JAP KC_LANGUAGE_1
 #define ENG KC_LANGUAGE_2
 
-#define CPI_INC CPI_I100
-#define CPI_DEC CPI_D100
 #define CLICK KC_MS_BTN1
 #define RCLICK KC_MS_BTN2
+
+#define SCREENSHOT_PART LCMD(LSFT(KC_4))
+#define SCREENSHOT_WHOLE LCMD(LSFT(KC_3))
 
 // macro
 
@@ -111,6 +112,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [3] = LAYOUT_universal (
+    XXXXXXX   , _______   , _______   , _______   , _______   , _______   ,                           _______   , _______   , _______   , _______   , _______   , XXXXXXX   ,
+    XXXXXXX   , _______   , _______   , _______   , _______   , SCREENSHOT_PART,                      _______   , _______   , _______   , _______   , _______   , XXXXXXX   ,
+    XXXXXXX   , _______   , _______   , _______   , _______   , SCREENSHOT_WHOLE,                     _______   , _______   , _______   , _______   , _______   , XXXXXXX   ,
+    XXXXXXX   , _______   , _______   ,             _______   , _______   ,                           _______   , _______   ,             _______   , _______   , XXXXXXX
+  ),
+
+  // auto mouse layer
+  [4] = LAYOUT_universal (
     XXXXXXX   , _______   , _______   , _______   , _______   , _______   ,                           _______   , _______   , _______   , _______   , _______   , XXXXXXX   ,
     XXXXXXX   , _______   , _______   , _______   , _______   , _______   ,                           _______   , CLICK     , RCLICK    , _______   , _______   , XXXXXXX   ,
     XXXXXXX   , _______   , _______   , _______   , _______   , _______   ,                           _______   , _______   , _______   , _______   , _______   , XXXXXXX   ,
