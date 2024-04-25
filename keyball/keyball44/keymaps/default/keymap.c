261021a4381d62861d17d61e47d9804aab2c5e68
yusukemorita <yusuke.ppq@gmail.com>
2024-04-25 18:32:03 +0200

    move cmd to home row

--- keyball/keyball44/keymaps/default/keymap.c
+++ keyball/keyball44/keymaps/default/keymap.c
@@ -92,14 +92,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // keymap for default
   [0] = LAYOUT_universal(
     XXXXXXX   , KC_Q      , KC_W      , KC_E      , KC_R      , KC_T      ,                          KC_Y      , KC_U      , KC_I      , KC_O      , KC_P      , XXXXXXX   ,
-    XXXXXXX   , KC_A      ,LCTL_T(KC_S),LALT_T(KC_D),KC_F     , KC_G      ,                          KC_H      , KC_J      ,LALT_T(KC_K),RCTL_T(KC_L),KC_COLON , XXXXXXX   ,
+    XXXXXXX   , KC_A      ,LCTL_T(KC_S),LALT_T(KC_D),LCMD_T(KC_F), KC_G   ,                          KC_H     ,LCMD_T(KC_J),LALT_T(KC_K),RCTL_T(KC_L),KC_COLON , XXXXXXX   ,
     XXXXXXX   , KC_Z      , KC_X      , KC_C      , KC_V      , KC_B      ,                          KC_N      , KC_M      , KC_COMM   , KC_DOT    , KC_SLASH  , XXXXXXX   ,
-    XXXXXXX   , XXXXXXX   ,             KC_LGUI   ,LT(1,CLICK), LSFT_T(KC_SPACE),                    RSFT_T(KC_ENT), LT(2,CLICK),        _______   , _______   , _______
+    XXXXXXX   , XXXXXXX   ,             CLICK     , MO(1)     , LSFT_T(KC_SPACE),                    RSFT_T(KC_ENT), MO(2) ,             _______   , _______   , _______
   ),
 
   [1] = LAYOUT_universal(
-    XXXXXXX   , _______   , _______   , _______   , KC_DEL    , KC_BSPC   ,                          KC_0     , KC_1     , KC_2     , KC_3     , _______  , XXXXXXX  ,
-    XXXXXXX   , _______   , _______   , _______   , KC_DEL    , KC_BSPC   ,                          _______  , KC_4     , KC_5     , KC_6     , _______  , XXXXXXX  ,
+    XXXXXXX   , _______   , _______   , _______   , _______   , _______   ,                          KC_0     , KC_1     , KC_2     , KC_3     , _______  , XXXXXXX  ,
+    XXXXXXX   , _______   , _______   , KC_DEL    , KC_BSPC   , _______   ,                          _______  , KC_4     , KC_5     , KC_6     , _______  , XXXXXXX  ,
     XXXXXXX   , _______   , KC_MINUS  , KC_EQUAL  , KC_LCBR   , KC_RCBR   ,                          _______  , KC_7     , KC_8     , KC_9     , _______  , XXXXXXX  ,
     _______   , _______   ,             _______   , _______   , _______   ,                    KC_ESC_AND_ENG , LT(3,RCLICK) ,        _______  , _______  , _______
   ),
