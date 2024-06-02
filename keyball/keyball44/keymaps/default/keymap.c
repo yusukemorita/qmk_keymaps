d9240664c8b8dc6bdfe3a4ba0d6328d950faea92
yusukemorita <yusuke.ppq@gmail.com>
2024-06-02 16:37:24 +0200

    try increasing threshold more

--- keyball/keyball44/keymaps/default/keymap.c
+++ keyball/keyball44/keymaps/default/keymap.c
@@ -49,7 +49,7 @@ enum custom_keycodes {
 // trigger by holding down a key
 bool switch_desktop_with_trackball = false;
 int switch_desktop_x_threshold = 160;
-int switch_desktop_y_threshold = 200;
+int switch_desktop_y_threshold = 300;
 
 bool switch_tabs_with_trackball = false;
 int switch_tabs_threshold = 160;
