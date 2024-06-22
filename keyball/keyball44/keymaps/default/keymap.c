f4ede905fdcc109f47c7c167d6b8344365a391d7
yusukemorita <yusuke.ppq@gmail.com>
2024-06-22 13:40:00 +0200

    delete auto_mouse_enable

--- keyball/keyball44/keymaps/default/keymap.c
+++ keyball/keyball44/keymaps/default/keymap.c
@@ -163,11 +163,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   return true;
 }
 
-// ref: https://docs.qmk.fm/#/feature_pointing_device?id=how-to-enable
-void keyboard_post_init_user(void) {
-    set_auto_mouse_enable(true);
-}
-
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // keymap for default
