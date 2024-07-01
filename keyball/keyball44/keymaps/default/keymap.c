4b1703e4eb138b79577cf762a9a44381426413fe
yusukemorita <yusuke.ppq@gmail.com>
2024-07-01 23:05:26 +0200

    add emoji shortcut

--- keyball/keyball44/keymaps/default/keymap.c
+++ keyball/keyball44/keymaps/default/keymap.c
@@ -40,6 +40,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 enum custom_keycodes {
   ESC_AND_ENG = SAFE_RANGE,
   HOLD_QK_BOOT,
+  EMOJI,
   EMAIL_1,
   EMAIL_2,
   EMAIL_3,
@@ -126,6 +127,13 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
       break;
 
+    case EMOJI:
+      if (record->event.pressed) {
+        // ctl + cmd + space for emoji
+        SEND_STRING(SS_DOWN(X_LCTL) SS_DELAY(1) SS_DOWN(X_GUI) SS_TAP(X_SPC) SS_DELAY(1) SS_UP(X_GUI) SS_DELAY(1) SS_UP(X_LCTL));
+      }
+      break;
+
     case EMAIL_1:
       if (record->event.pressed) {
         SEND_STRING(ENV_EMAIL_1);
@@ -160,7 +168,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   // (mostly) numbers and shortcuts
   [1] = LAYOUT_universal(
-    XXXXXXX   , _______   , _______   , L_TAB     , R_TAB     , _______   ,                          KC_0     , KC_1     , KC_2     , KC_3     , _______  , XXXXXXX,
+    XXXXXXX   , _______   , EMOJI     , L_TAB     , R_TAB     , _______   ,                          KC_0     , KC_1     , KC_2     , KC_3     , _______  , XXXXXXX,
     XXXXXXX   , _______   , KC_LCBR   , KC_DEL    , KC_BSPC   , KC_RCBR   ,                          KC_MINUS , KC_4     , KC_5     , KC_6     , _______  , XXXXXXX,
     XXXXXXX   , _______   , _______   , _______   , _______   , _______   ,                          KC_EQUAL , KC_7     , KC_8     , KC_9     , _______  , XXXXXXX,
     XXXXXXX   , _______   ,             _______   , _______   , HOLD_QK_BOOT,                     ESC_AND_ENG , MO(3)    ,            _______  , _______  , _______
