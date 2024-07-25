5c87caff4b3859ab5e39de44e305bbba4668efde
yusukemorita <yusuke.ppq@gmail.com>
2024-07-25 20:51:26 +0200

    add button for toggling LEDs

--- cocot38mini/keymaps/vial/keymap.c
+++ cocot38mini/keymaps/vial/keymap.c
@@ -32,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,             KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
         KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_ESC,  KC_H,    KC_J,    KC_K,    KC_L,    KC_MINS,
         KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,             KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
-                          KC_LALT, KC_LNG2, KC_SPC,  MS_BTN1, KC_ENT,  KC_LNG1, HOLD_QK_BOOT
+                          KC_LALT, KC_LNG2, KC_SPC,  MS_BTN1, KC_ENT,  BL_TOGG, HOLD_QK_BOOT
     ),
 
     [1] = LAYOUT(
