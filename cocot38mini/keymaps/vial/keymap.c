fda814a9058a1317a197b3ba7b0da87aa582c558
yusukemorita <yusuke.ppq@gmail.com>
2024-07-26 20:29:10 +0200

    try moving QK_BOOT

--- cocot38mini/keymaps/vial/keymap.c
+++ cocot38mini/keymaps/vial/keymap.c
@@ -31,8 +31,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,             KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
         KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_ESC,  KC_H,    KC_J,    KC_K,    KC_L,    KC_MINS,
-        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,             KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
-                          KC_LALT, KC_LNG2, KC_SPC,  MS_BTN1, KC_ENT,  RGB_TOG, HOLD_QK_BOOT
+        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,             KC_N,    KC_M,    KC_COMM, KC_DOT,  HOLD_QK_BOOT,
+                          KC_LALT, KC_LNG2, KC_SPC,  MS_BTN1, KC_ENT,  RGB_TOG, KC_SLSH
     ),
 
     [1] = LAYOUT(
