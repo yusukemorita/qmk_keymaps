a59d747b88a503de9a32015dd3acb1f7faceb980
yusukemorita <yusuke.ppq@gmail.com>
2024-08-03 23:37:34 +0200

    try adding enter and layer 2 keys

--- cocot38mini/keymaps/vial/keymap.c
+++ cocot38mini/keymaps/vial/keymap.c
@@ -34,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,             KC_N,    KC_M,    KC_COMM, KC_DOT,  HOLD_QK_BOOT,
                           KC_1,    MO(1),   KC_3,
                                                     KC_4, // key above trackball
-                                                    KC_5,    KC_6,    KC_7,    KC_8
+                                                    KC_5,     KC_ENT,  MO(2),    KC_8
     ),
 
     [1] = LAYOUT(
