6d381ed5b669c743b4728d8ddd58107167269a89
yusukemorita <yusuke.ppq@gmail.com>
2024-08-18 11:12:53 +0200

    add debug mode

--- cocot38mini/keymaps/vial/keymap.c
+++ cocot38mini/keymaps/vial/keymap.c
@@ -72,7 +72,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, L_TAB  , R_TAB  , _______,          KC_0    ,KC_1,    KC_2,    KC_3,    _______,
         _______, KC_LCBR, KC_DEL , KC_BSPC, KC_RCBR,          KC_MINUS,KC_4,    KC_5,    KC_6,    _______,
         _______, KC_QUES, _______, _______, _______,          KC_EQUAL,KC_7,    KC_8,    KC_9,   KC_SLASH,
-                          _______, _______, _______, 
+                          DB_TOGG, _______, _______,
                                                      _______, // key above trackball
                                                      _______, ESC_AND_ENG, MO(3), _______
     ),
