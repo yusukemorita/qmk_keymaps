f74b926b6ff83c31611e5e0fa91e5a8fe36fae54
yusukemorita <yusuke.ppq@gmail.com>
2024-09-15 13:45:20 +0200

    add RCLICK to layer 1

--- cocot38mini/keymaps/vial/keymap.c
+++ cocot38mini/keymaps/vial/keymap.c
@@ -126,7 +126,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, KC_LCBR, KC_DEL , KC_BSPC, KC_RCBR,          KC_MINUS,KC_4,    KC_5,    KC_6,    _______,
         _______, KC_QUES, _______, _______, _______,          KC_EQUAL,KC_7,    KC_8,    KC_9,   KC_SLASH,
                           _______, _______, _______, 
-                                                     _______, // key above trackball
+                                                      RCLICK, // key above trackball
                                                      _______, ESC_AND_ENG, MO(3), _______
     ),
 
