17800b63ee5292a2dce630fe0f08bec74cb3e5d1
yusukemorita <yusuke.ppq@gmail.com>
2024-12-22 00:08:59 +0100

    delete more stuff

--- trackpoint/config.h
+++ trackpoint/config.h
@@ -1,10 +1,1 @@
 #pragma once
-
-/* disable action features */
-//#define NO_ACTION_LAYER
-//#define NO_ACTION_TAPPING
-//#define NO_ACTION_ONESHOT
-
-#define ADNS5050_SCLK_PIN         B7
-#define ADNS5050_SDIO_PIN         C6
-#define ADNS5050_CS_PIN           B4
--- trackpoint/trackpoint.h
+++ trackpoint/trackpoint.h
@@ -1,18 +1,3 @@
 #pragma once
 
 #include "quantum.h"
-
-typedef union {
-  uint32_t raw;
-  struct {
-    uint8_t dpi_config;
-  };
-} keyboard_config_t;
-
-extern keyboard_config_t keyboard_config;
-
-enum ploopy_keycodes {
-    DPI_CONFIG = QK_KB_0,
-};
-
-void cycle_dpi(void);
