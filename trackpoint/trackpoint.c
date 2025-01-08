422c7ccd79eac24477e016aa9d40df73b4789eaa
yusukemorita <yusuke.ppq@gmail.com>
2025-01-08 23:30:13 +0100

    try printing buttons

--- trackpoint/trackpoint.c
+++ trackpoint/trackpoint.c
@@ -17,14 +17,14 @@
 #include "trackpoint.h"
 #include "wait.h"
 
-// void ps2_mouse_moved_user(report_mouse_t *mouse_report) {
+void ps2_mouse_moved_user(report_mouse_t *mouse_report) {
     // Print a message to indicate the function is called
     // print("Mouse moved\n");
 
     // Print each field in the mouse_report structure
     // dprintf("  x: %d\n", mouse_report->x);
     // dprintf("  y: %d\n", mouse_report->y);
-    // dprintf("  wheel: %d\n", mouse_report->h);
-    // dprintf("  horizontal wheel: %d\n", mouse_report->v);
-    // dprintf("  buttons: 0x%02X\n", mouse_report->buttons);
-// }
+    dprintf("Mouse moved! buttons: 0x%02X\n", mouse_report->buttons);
+    dprintf("  wheel: %d\n", mouse_report->h);
+    dprintf("  horizontal wheel: %d\n", mouse_report->v);
+}
