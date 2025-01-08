63cda1ddc03101e1bbf2eb0ec83899991b7385af
yusukemorita <yusuke.ppq@gmail.com>
2025-01-08 23:42:01 +0100

    enable debug mode

--- trackpoint/trackpoint.c
+++ trackpoint/trackpoint.c
@@ -19,12 +19,12 @@
 
 void ps2_mouse_moved_user(report_mouse_t *mouse_report) {
     // Print a message to indicate the function is called
-    // print("Mouse moved\n");
+    print("Mouse moved\n");
 
     // Print each field in the mouse_report structure
-    // dprintf("  x: %d\n", mouse_report->x);
-    // dprintf("  y: %d\n", mouse_report->y);
-    uprintf("Mouse moved! buttons: 0x%02X\n", mouse_report->buttons);
+    uprintf("  x: %d\n", mouse_report->x);
+    uprintf("  y: %d\n", mouse_report->y);
+    uprintf("  buttons: 0x%02X\n", mouse_report->buttons);
     uprintf("  wheel: %d\n", mouse_report->h);
     uprintf("  horizontal wheel: %d\n", mouse_report->v);
 }
