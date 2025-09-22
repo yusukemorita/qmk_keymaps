414c807a4e98972d30ba8f2308ea3fabee8b2168
yusukemorita <yusuke.ppq@gmail.com>
2025-09-22 23:14:18 +0200

    CAPS_WORD_ENABLE

--- crkbd-trackpoint/keymaps/via/rules.mk
+++ crkbd-trackpoint/keymaps/via/rules.mk
@@ -1,6 +1,8 @@
-MOUSEKEY_ENABLE = no     # Mouse keys
-VIA_ENABLE      = yes    # Enable VIA
-CONSOLE_ENABLE  = yes    # Console for debug
+MOUSEKEY_ENABLE  = no    # Mouse keys
+VIA_ENABLE       = yes   # Enable VIA
+CONSOLE_ENABLE   = yes   # Console for debug
+CAPS_WORD_ENABLE = yes   # required for enabling CW_TOGG key
+
 
 # `PERSONAL_EMAIL`, `PERSONAL_EMAIL_DEV`, `WORK_EMAIL`
 # should be defined as env variables at compile time
